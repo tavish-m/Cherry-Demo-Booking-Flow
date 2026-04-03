3d63a3ef92b7bf7a951e665baa12ec3c3a437c2a
Tavish Monroe <tavish@withcherry.com>
2026-04-03 13:57:48 -0700

    Add node-fetch for Render compatibility

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const fetch = require('node-fetch');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
